9b6ee55c73a56d450c63ff99e7a227c617ef5a4b
Carlos Silva <carlos@octet.pt>
2021-04-03 02:50:03 +0100

    Adds Banktivity 7

---
+++ Casks/banktivity7.rb
@@ -0,0 +1,15 @@
+cask "banktivity7" do
+  version "7.5.3"
+  sha256 "fec905834853295ec1223894e0109d4e8edd0c7d682be4439e33633e4c526e1d"
+
+  # iggsoft.com/ was verified as official when first introduced to the cask
+  url "https://www.iggsoft.com/banktivity/Banktivity#{version.major}_Web.dmg"
+  appcast "https://www.iggsoftware.com/product-versions.json"
+  name "Banktivity"
+  desc "App to manage bank accounts in one place"
+  homepage "https://www.iggsoftware.com/banktivity/"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Banktivity #{version.major}.app"
+end
