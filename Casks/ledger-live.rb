1888b0660346ec62b266e683519f0c7e959f744d
Carlos Silva <carlos@octet.pt>
2022-04-09 04:32:12 +0100

    Adds ledger live

---
+++ Casks/ledger-live.rb
@@ -0,0 +1,30 @@
+cask "ledger-live" do
+  version "2.40.2"
+  sha256 "75ace28bdd01aaf2740a67a6460d01a88460d82a3041304d5503426ef33991ad"
+
+  url "https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v#{version}/ledger-live-desktop-#{version}-mac.dmg",
+      verified: "github.com/LedgerHQ/ledger-live-desktop/"
+  name "Ledger Live"
+  desc "Wallet desktop application to maintain multiple cryptocurrencies"
+  homepage "https://www.ledgerwallet.com/live"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "Ledger Live.app"
+
+  uninstall quit: [
+    "com.ledger.live",
+    "com.ledger.live.helper",
+  ]
+
+  zap trash: [
+    "~/Library/Application Support/Ledger Live",
+    "~/Library/Preferences/com.ledger.live.plist",
+    "~/Library/Saved Application State/com.ledger.live.savedState",
+  ]
+end
